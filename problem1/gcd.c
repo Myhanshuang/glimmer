8371bdff31a1af3be01b7e747fe83a5ec1618a2c
My_hanshuang <2774994572@qq.com>
2024-09-20 08:12:52 +0800

    change gcd nm

--- problem1/gcd.c
+++ problem1/gcd.c
@@ -3,7 +3,7 @@ int gcd(int a, int b){
     return b ? gcd(b, a % b) : a;
 }
 int main(){
-    int a, b;
-    scanf("%d %d", & a, & b);
-    printf("%d", gcd(a, b));
+    int m, n;
+    scanf("%d %d", & m, & n);
+    printf("%d", gcd(m, n));
 }
